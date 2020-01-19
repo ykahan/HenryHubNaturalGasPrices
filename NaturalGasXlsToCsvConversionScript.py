ca2a2f5b297d6618ce553caf6909168573d752be
Jatudrei <yehoshua.kahan@gmail.com>
2020-01-19 11:59:11 +0200

    Encapsulated functionality in methods

--- NaturalGasXlsToCsvConversionScript.py
+++ NaturalGasXlsToCsvConversionScript.py
@@ -5,11 +5,26 @@ from os import path, remove, rename, listdir
 import pandas as pd
 from selenium import webdriver
 
+
+def remove_files(provided_path, provided_name):
+    for file in listdir(provided_path):
+        if file.startswith(provided_name):
+            remove(downloadsPath + file)
+
+
+def prepare_driver(path_to_chromedriver, seconds):
+    drive = webdriver.Chrome(path_to_chromedriver)
+    drive.implicitly_wait(seconds)
+    return drive
+
+
 source = "C:\\Users\\USER\\Java_Workspace_Yehoshua\\Selenium Dependencies"
 source += "\\chromedriver.exe"
-driver = webdriver.Chrome(source)
+wait = 10
+
+driver = prepare_driver(source, wait)
+
 target = "http://www.eia.gov/dnav/ng/hist/rngwhhdm.htm"
-driver.implicitly_wait(10)
 driver.get(target)
 
 dailyPricesXpath = "//a[contains(@href, 'whhdD') and contains(@class, 'Nav')]"
@@ -17,80 +32,101 @@ weeklyPricesXpath = "//a[contains(@href, 'whhdW') and contains(@class, 'Nav')]"
 monthlyPricesXpath = "//a[contains(@href, 'whhdM') and contains(@class, 'Nav')]"
 yearlyPricesXpath = "//a[contains(@href, 'whhdA') and contains(@class, 'Nav')]"
 
-dailyPrices = driver.find_element_by_xpath(dailyPricesXpath)
-weeklyPrices = driver.find_element_by_xpath(weeklyPricesXpath)
-monthlyPrices = driver.find_element_by_xpath(monthlyPricesXpath)
-yearlyPrices = driver.find_element_by_xpath(yearlyPricesXpath)
-
 downloadsPath = "C:\\Users\\USER\\Downloads\\"
+commonName = "RNGWHH"
 
-for file in listdir(downloadsPath):
-    if file.startswith("RNGWHH"):
-        remove(file)
+remove_files(downloadsPath, commonName)
 
 datasets = [dailyPricesXpath, weeklyPricesXpath, monthlyPricesXpath, yearlyPricesXpath]
+
+
+def download_file(xpath):
+    download_link = driver.find_element_by_xpath(xpath)
+    download_link.click()
+
+
 for dataset in datasets:
     target = driver.find_element_by_xpath(dataset)
     target.click()
 
     downloadXpath = "//a[contains(text(), 'Download')]"
-    downloadLink = driver.find_element_by_xpath(downloadXpath)
-    downloadLink.click()
+    download_file(downloadXpath)
 
     time.sleep(5)
 
-url = driver.current_url
 driver.quit()
+
 annualDataName = "RNGWHHDa"
 monthlyDataName = "RNGWHHDm"
 weeklyDataName = "RNGWHHDw"
 dailyDataName = "RNGWHHDd"
 
-names = [annualDataName, monthlyDataName, weeklyDataName, dailyDataName]
+old_names = [annualDataName, monthlyDataName, weeklyDataName, dailyDataName]
 
-for name in names:
-    excel = name + ".xls"
-    newExcel = ""
-    if name.endswith("a"):
-        newExcel = "annual.xls"
-        csv = "annual"
-    elif name.endswith("w"):
-        newExcel = "weekly.xls"
-        csv = "weekly"
-    elif name.endswith("d"):
-        newExcel = "daily.xls"
-        csv = "daily"
+
+def get_file_names(old_name):
+    # global excel, newExcel, csv
+    exc = old_name + ".xls"
+    if old_name.endswith("a"):
+        new_excel = "annual.xls"
+        comma_sep_values = "annual"
+    elif old_name.endswith("w"):
+        new_excel = "weekly.xls"
+        comma_sep_values = "weekly"
+    elif old_name.endswith("d"):
+        new_excel = "daily.xls"
+        comma_sep_values = "daily"
     else:
-        newExcel = "monthly.xls"
-        csv = "monthly"
-    csv += ".csv"
+        new_excel = "monthly.xls"
+        comma_sep_values = "monthly"
+    comma_sep_values += ".csv"
+    return [exc, new_excel, comma_sep_values]
+
+
+def move_file(path_exists, src, destination):
+    if path_exists:
+        remove(destination)
+    shutil.move(src, destination)
+
+
+def rename_file(path_exists, old_name, new_name):
+    if path_exists:
+        remove(new_name)
+    rename(old_name, new_name)
+
+
+def convert_from_xls_to_csv(local_excel, local_sheet, csv_path):
+    read_file = pd.read_excel(local_excel, local_sheet)
+    read_file.to_csv(csv_path, index=None, header=True)
+
+
+for name in old_names:
+    new_names = get_file_names(name)
+    excel = new_names[0]
+    newExcel = new_names[1]
+    csv = new_names[2]
 
     source = downloadsPath + excel
     destinationExcel = "C:\\Users\\USER\\Documents\\Yehoshua\\Programming\\Python\\HenryHubNaturalGasPricesOld" \
                        "\\XlsFiles\\"
-    destinationCsv = "C:\\Users\\USER\\Documents\\Yehoshua\\Programming\\Python\\HenryHubNaturalGasPricesOld" \
-                     "\\CsvFiles\\"
 
     excelPath = destinationExcel + "\\" + excel
     newExcelPath = destinationExcel + "\\" + newExcel
+
+    destinationCsv = "C:\\Users\\USER\\Documents\\Yehoshua\\Programming\\Python\\HenryHubNaturalGasPricesOld" \
+                     "\\CsvFiles\\"
     csvPath = destinationCsv + csv
 
     excelPathExists = path.exists(excelPath)
     newExcelPathExists = path.exists(newExcelPath)
     csvPathExists = path.exists(csvPath)
 
-    if excelPathExists:
-        remove(excelPath)
+    move_file(excelPathExists, source, excelPath)
 
-    shutil.move(source, destinationExcel)
-
-    if newExcelPathExists:
-        remove(newExcelPath)
-
-    rename(excelPath, newExcelPath)
-    read_file = pd.read_excel(newExcelPath, "Data 1")
+    rename_file(newExcelPathExists, excelPath, newExcelPath)
 
     if csvPathExists:
         remove(csvPath)
 
-    read_file.to_csv(csvPath, index=None, header=True)
+    sheet = "Data 1"
+    convert_from_xls_to_csv(newExcelPath, sheet, csvPath)
